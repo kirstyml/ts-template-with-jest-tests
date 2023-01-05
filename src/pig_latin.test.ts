489fd0714eec07562ce7a123b593ee2e43c9e9ed
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-01-05 09:40:03 +0000

    two word case and punctuation

--- src/pig_latin.test.ts
+++ src/pig_latin.test.ts
@@ -1,12 +1,24 @@
 import { PigLatin } from "../src/pig_latin";
 describe("empty case", () => {
-  it("returns an empty sting when given an empty string", () => {
-    expect(PigLatin("")).toBe("");
-  });
+    it("returns an empty sting when given an empty string", () => {
+        expect(PigLatin("")).toBe("");
+    });
 });
 
 describe("one word case", () => {
-    it("returns elloHay for hello", () => {
-      expect(PigLatin("Hello")).toBe("elloHay");
+    it("returns elloHay for Hello", () => {
+        expect(PigLatin("Hello")).toBe("elloHay");
     });
-  });
+});
+
+describe("two word case", () => {
+    it("returns elloHay orldWay for Hello World", () => {
+        expect(PigLatin("Hello World")).toBe("elloHay orldWay");
+    });
+});
+
+describe("leaves puntuation untouched", () => {
+    it("returns elloHay orldWay ! for Hello World !", () => {
+        expect(PigLatin("Hello World !")).toBe("elloHay orldWay !");
+    });
+});
--- src/pig_latin.ts
+++ src/pig_latin.ts
@@ -1,8 +1,14 @@
-export function PigLatin(sentence : string) : string {
-    // remove the first letter and add it to the end of the word
-    // add ay to the end of the word
+export function PigLatin(sentence: string): string {
     if (sentence.length > 0) {
-        return `${sentence.slice(1)}${sentence[0]}ay`;
+        // need to split the string by spaces
+        const words = sentence.split(" ");
+        //for each item in the words array:
+        // if it is not
+        // remove the first letter and add it to the end of the word, if it is longer than 1 character
+        // add ay to the end of the word if it is not punctuation
+        const newWords = words.map(word => `${word.length > 1 ? word.slice(1) : ""}${word[0]}${word.match(/\W+/) ? "" : "ay"}`);
+        // place new words back into sentence
+        return newWords.join(" ");
     }
     return sentence;
 }
